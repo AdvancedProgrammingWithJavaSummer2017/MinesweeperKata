476ee6a727bb40d837b35f9d1aa9c30581d23310
Bin Gao <gaobin@Bins-MacBook-Pro.local>
2017-07-26 20:25:15 -0700

    request for comments

--- minesweeper/src/main/java/edu/pdx/cs410J/katas/Minesweeper.java
+++ minesweeper/src/main/java/edu/pdx/cs410J/katas/Minesweeper.java
@@ -1,16 +1,45 @@
 package edu.pdx.cs410J.katas;
 
 public class Minesweeper  {
-
-  public Minesweeper(int row, int column) {
-
+  private char[][] board;
+  private int row;
+  private int col;
+  private int currentEmptyRow;
+  public Minesweeper(int row, int col) throws ZeroSizedBoardException {
+    if (row * col == 0) {
+      throw new ZeroSizedBoardException();
+    }
+    this.row = row;
+    this.col = col;
+    board = new char[row][col];
   }
 
-  public void addRow(String row) {
-
+  public void addRow(String row) throws RaggedBoardException {
+    if (row.length() != board[0].length) {
+      throw new RaggedBoardException();
+    }
+    for (int i = 0; i < col; i++) {
+      board[currentEmptyRow][i] = row.charAt(i);
+    }
+    currentEmptyRow++;
   }
 
   public String solve() {
-    return null;
+
+
+
+
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < row; i++) {
+      for (int j = 0; j < col; j++) {
+        sb.append(board[i][j]);
+      }
+      if (i != row - 1) {
+        sb.append("\n");
+      }
+    }
+    return sb.toString();
   }
+
+
 }
