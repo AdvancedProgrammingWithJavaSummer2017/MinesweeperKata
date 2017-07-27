6373a92bf7e91c5f4979f457e4b435b2480a990c
Niveditha Venugopal <nivedit@pdx.edu>
2017-07-26 20:20:53 -0700

    Changes made by Liz and Niveditha

--- minesweeper/src/main/java/edu/pdx/cs410J/katas/Minesweeper.java
+++ minesweeper/src/main/java/edu/pdx/cs410J/katas/Minesweeper.java
@@ -1,16 +1,41 @@
 package edu.pdx.cs410J.katas;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Minesweeper  {
+  private int row, column;
+  private List<Character> board = new ArrayList<>();
 
-  public Minesweeper(int row, int column) {
-
+  public Minesweeper(int row, int column) throws ZeroSizedBoardException{
+    if(row == 0 || column == 0){
+      throw new ZeroSizedBoardException();
+    }
+    this.row = row;
+    this.column = column;
   }
 
-  public void addRow(String row) {
+  public void addRow(String row) throws RaggedBoardException{
+
+    if(row.length() != this.column){
+      throw new RaggedBoardException();
+    }
+    for(int i = 0; i<row.length(); i++){
+      this.board.add(row.charAt(i));
+    }
 
   }
 
   public String solve() {
-    return null;
+    String solved = "";
+    for(char c: this.board){
+      if(c == '*'){
+        solved += "*";
+      }
+      else if(c == '.'){
+        solved += "0";
+      }
+    }
+    return solved;
   }
 }
